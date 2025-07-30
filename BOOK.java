7b23e946ce8781b23de3646ee0a9f4040a3bc618
Sanjay Kumar B <sanjayboya2005@gmail.com>
2025-07-30 21:19:09 +0530

    Add files via upload

---
+++ BOOK.java
@@ -0,0 +1,96 @@
+public class BOOK {
+     public static void main(String[] args) {
+        Author author = new Author("Adam Das", "ad@mgmail.com", 'M');
+
+        Book book = new Book("Half life", author, 299.99, 210);
+
+        System.out.println(book);
+    }
+}
+
+class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    public String toString() {
+        return "Author Name: " + name + ", Email: " + email + ", Gender: " + gender;
+    }
+}
+
+class Book {
+    private String name;
+    private Author author; 
+    private double price;
+    private int qtyInStock;
+
+    public Book(String name, Author author, double price, int qtyInStock) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qtyInStock = qtyInStock;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQtyInStock() {
+        return qtyInStock;
+    }
+
+    public void setQtyInStock(int qtyInStock) {
+        this.qtyInStock = qtyInStock;
+    }
+
+    public String toString() {
+        return "Book Name: " + name +
+               "\nPrice: ₹" + price +
+               "\nQuantity in Stock: " + qtyInStock +
+               "\n" + author.toString();
+    }
+}
